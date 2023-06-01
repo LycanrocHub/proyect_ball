f7159acc118c35b5f2666b5c69c26f063474a333
Lycanroc <juanluis.casado@escuelaestech.es>
2023-06-01 18:13:58 +0200

    Pendiente de terminar las animaciones del login

---
+++ prueba api/app/src/main/java/com/example/pikatech/ui/LoginFragment.kt
@@ -0,0 +1,35 @@
+package com.example.pikatech.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.TranslateAnimation
+import com.example.pikatech.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val animacion1 = TranslateAnimation(0.0f, 0.0f, -1600.0f, 0.0f)
+        animacion1.setDuration(1000)
+
+        val animacion2 = TranslateAnimation(-1600f, 0.0f, 0.0f, 0.0f)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+
+
+    }
+
+}
