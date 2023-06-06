79457cee869836ff40ec357114d3b2423e62a83a
enrique.gomez <enrique.gomez@escuelaestech.es>
2023-06-06 12:06:23 +0200

    Enrique V5

--- prueba api/app/src/main/java/com/example/pikatech/ui/AdapterItems.kt
+++ prueba api/app/src/main/java/com/example/pikatech/ui/AdapterItems.kt
@@ -2,6 +2,7 @@ package com.example.pikatech.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
 import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -58,5 +59,7 @@ class AdapterItems(val myViewModel : MyViewModel, val lifeCycle: LifecycleOwner)
         } }
 
     }
+
+
 
 }
